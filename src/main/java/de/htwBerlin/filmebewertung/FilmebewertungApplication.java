a916d708f0c39b9af4f0c7188a97d631233c2d49
s0569073 <Robin.Pankotsch@Student.HTW-Berlin.de>
2022-10-22 12:09:25 +0200

    First Commit

---
+++ src/main/java/de/htwBerlin/filmebewertung/FilmebewertungApplication.java
@@ -0,0 +1,13 @@
+package de.htwBerlin.filmebewertung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmebewertungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmebewertungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwBerlin/filmebewertung/FilmebewertungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwBerlin.filmebewertung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmebewertungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
